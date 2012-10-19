ce2fdc4e0821d74652a3a4cd7cef10325b361f71
Sonny Tambiac <ynnossence@gmail.com>
2012-10-19 16:24:54 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootSlim::Application.config.secret_token = '004ee77947bec8ec34a822e000df2d60930e1f349f17d818a352654e2ccb8bbd6d126cae3c6d9f233e61b13417944226f4571f46ea5bb612e259beadaa1bf5cc'
